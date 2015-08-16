ef261e4b00936446423ab1cae196d3a34d42c4c1
JesseJesseppi <jesse.creech@gmail.com>
2015-08-16 19:09:28 +1000

    associative array added to facilitate showing employees names as well as block costs

--- timesheet_app.rb
+++ timesheet_app.rb
@@ -3,6 +3,8 @@
 	@EMPLOYEES_REQUIRED_SHIFT = [4,5,4,8,8,5,6,9,4,7]
 	@EMPLOYEE_SHIFT_BLOCK_COST = [71.16, 95.50, 69.16, 147.76, 138.32, 86.45, 103.74, 171.90, 73.88, 140.91]
 
+	@ASSOCIATIVE_EMPLOYEE_SHIFT_BLOCK_COST = {"Alex" => 71.16, "Jake" => 95.50, "Tasmin" => 69.16, "Josh" => 147.76, "Adam" => 138.32, "Wayne" => 86.45, "Dave" => 103.74, "Bri" => 171.90, "Scott" => 73.88, "Marissa" 	=> 140.91}
+
 	@BUDGET = 800.00
 
 	def print_arrays
@@ -42,9 +44,11 @@
 		end	
 	end
 
+	$closest_to_budget = 0
+
 # print_arrays
 
-	def recursive_addition(numbers, budget, numbersAdded =[])
+	def simple_recursive_addition(numbers, budget, numbersAdded =[])
 
 		recursive_sum = numbersAdded.inject(0, :+)
 		# puts recursive_sum
@@ -58,6 +62,9 @@
   					print "    ....... "
   					print recursive_sum
   					puts "got to here"
+  					if recursive_sum > $closest_to_budget
+  						$closest_to_budget = recursive_sum
+  					end
   				end
   				return
   			end
@@ -66,8 +73,52 @@
   		(0..(numbers.length - 1)).each do |i|
     		n = numbers[i]
     		remaining = numbers.drop(i+1)
-    		recursive_addition(remaining, budget, numbersAdded + [n])
+    		simple_recursive_addition(remaining, budget, numbersAdded + [n])
   		end
 	end
 
-	recursive_addition(@EMPLOYEE_SHIFT_BLOCK_COST,@BUDGET)
+	$associative_closest_to_budget = 0
+
+	def associative_recursive_addition(numbers, budget, numbersAdded =[])
+
+		recursive_sum = numbersAdded.inject(0, :+)
+		
+  		if recursive_sum > 0
+  			if recursive_sum > 790 
+  				if recursive_sum <= budget
+  					print numbersAdded
+  					print "    ....... "
+  					print recursive_sum
+  					puts 
+  					if recursive_sum > $associative_closest_to_budget
+  						$associative_closest_to_budget = recursive_sum
+  					end
+  				end
+  				return
+  			end
+  		end
+	
+  		numbers.each_with_index do |(k,v),index|
+    		# puts"#{index}"
+    		# puts k
+    		# puts v
+    		n = v
+    		remaining = numbers.drop(index+1)
+    		associative_recursive_addition(remaining, budget, numbersAdded + [n])
+  		end
+	end	
+
+	simple_recursive_addition(@EMPLOYEE_SHIFT_BLOCK_COST,@BUDGET)
+	puts("the closest combination to budget amounted to: ")
+  	print $closest_to_budget
+  	puts
+  	associative_recursive_addition(@ASSOCIATIVE_EMPLOYEE_SHIFT_BLOCK_COST,@BUDGET)
+  	puts("the closest combination to budget amounted to: ")
+  	print $associative_closest_to_budget
+  	puts
+
+
+
+
+
+
